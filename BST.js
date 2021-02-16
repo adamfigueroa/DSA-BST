e701bf205ac71d7b231c54aa2e7e80ea3a874b40
Adam Figueroa <admiralfiggins@gmail.com>
2021-02-16 08:45:43 -0800

    question 2 complete

---
+++ BST.js
@@ -0,0 +1,50 @@
+// Question 1 Draw a BST
+// Given the data 3,1,4,6,9,2,5,7, if you were to insert this into an empty binary search tree, what would the tree look like? (Draw the tree, no coding needed here.)
+// Draw the BST with the keys - E A S Y Q U E S T I O N
+
+            //     3
+            //    / \
+            //   1   4
+            //    \   \
+            //     2   6
+            //        / \
+            //       7   9
+
+        //         E
+        //     /      \
+        //    S         A
+        //   / \       /
+        //  Y    Q     E
+        //  \   / \
+        //  U  S   I
+        //  \     /
+        //  T    O
+        //      /
+        //     N
+
+// Question 2 Remove the root
+// Show how the above trees would look like if you deleted the root of each tree. (Draw the trees, no coding needed here.)
+
+            //     4
+            //    / \
+            //   1    --- 6
+            //    \      / \
+            //     2    7   9
+            //        
+            //       
+
+
+
+
+        //         E
+        //     /      \
+        //    S         A
+        //   / \       
+        //  Y    Q     
+        //  \   / \
+        //  U  S   I
+        //  \     /
+        //  T    O
+        //      /
+        //     N
+
